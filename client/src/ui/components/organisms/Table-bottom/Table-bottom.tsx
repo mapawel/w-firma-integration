caae9ef2db13bf8399302c54617e12b8f49c3aaf
mapawel <pawelma@gmail.com>
2023-06-19 00:08:43 +0200

    refactor(front): pagination hook

--- client/src/ui/components/organisms/Table-bottom/Table-bottom.tsx
+++ client/src/ui/components/organisms/Table-bottom/Table-bottom.tsx
@@ -1,22 +1,30 @@
 import { FC } from 'react';
-import { useDataAndDataFilters } from '@/data-providers/filters-data/use-data-and-data-filters';
 import Select from 'react-select';
 import { selectStyle } from '../Table-top-header/select-style';
 import { ReactComponent as Prev } from '@/assets/icons/prev.svg';
 import { ReactComponent as Next } from '@/assets/icons/next.svg';
+import { usePagination } from '@/data-providers/pagination/use-pagination';
 
 export const TableBottom: FC = () => {
-    const { records, count, setRecords, skip, setSkip } =
-        useDataAndDataFilters();
-    const lastPageNo = Math.ceil(count / records);
+    const {
+        count,
+        setRecords,
+        lastPageNo,
+        handleNext,
+        handlePrev,
+        handleFirst,
+        handleLast,
+        range,
+        options,
+        isMoreThanTwo,
+        isMoreThanOne,
+    } = usePagination();
 
     return (
         <nav className="flex flex-col items-start justify-between space-y-3 p-4 md:flex-row md:items-center md:space-y-0">
             <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
                 <span className="font-semibold text-gray-900 dark:text-white">
-                    {`${skip + 1} - ${
-                        skip + records > count ? count : skip + records
-                    } `}
+                    {range}
                 </span>
                 {'z '}
                 <span className="font-semibold text-gray-900 dark:text-white">
@@ -25,23 +33,13 @@ export const TableBottom: FC = () => {
             </span>
             <form>
                 <Select
-                    options={[
-                        {
-                            value: 25,
-                            label: '25',
-                        },
-                        {
-                            value: 50,
-                            label: '50',
-                        },
-                    ]}
+                    options={options}
                     defaultValue={{
                         value: 50,
                         label: '50',
                     }}
                     className="w-full"
                     name="status"
-                    placeholder="Rekordów na stronę..."
                     styles={selectStyle}
                     onChange={(selected) => setRecords(selected?.value || 50)}
                 />
@@ -49,9 +47,7 @@ export const TableBottom: FC = () => {
             <ul className="inline-flex items-stretch -space-x-px">
                 <li>
                     <button
-                        onClick={() => {
-                            setSkip(skip === 0 ? skip : skip - records);
-                        }}
+                        onClick={handlePrev}
                         className="ml-0 flex h-full items-center justify-center rounded-l-lg border border-gray-300 bg-white px-3 py-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                     >
                         <Prev className="h-5 w-5 fill-secondary" />
@@ -60,28 +56,24 @@ export const TableBottom: FC = () => {
 
                 <li>
                     <button
-                        onClick={() => {
-                            setSkip(0);
-                        }}
+                        onClick={handleFirst}
                         className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                     >
                         1
                     </button>
                 </li>
 
-                {count / records > 2 && (
+                {isMoreThanTwo && (
                     <li>
                         <button className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                             ...
                         </button>
                     </li>
                 )}
-                {count / records > 1 && (
+                {isMoreThanOne && (
                     <li>
                         <button
-                            onClick={() => {
-                                setSkip((lastPageNo - 1) * records);
-                            }}
+                            onClick={handleLast}
                             className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                             {lastPageNo}
@@ -91,13 +83,7 @@ export const TableBottom: FC = () => {
 
                 <li>
                     <button
-                        onClick={() => {
-                            setSkip(
-                                skip === (lastPageNo - 1) * records
-                                    ? skip
-                                    : skip + records,
-                            );
-                        }}
+                        onClick={handleNext}
                         className="flex h-full items-center justify-center rounded-r-lg border border-gray-300 bg-white px-3 py-1.5 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                     >
                         <Next className="h-5 w-5 fill-secondary" />
