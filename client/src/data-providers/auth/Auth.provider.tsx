747f48abaf0cda6869fb8e86a7d9390624a5194c
mapawel <pawelma@gmail.com>
2023-08-07 22:52:54 +0200

    fix(front): auth-settings

--- client/src/data-providers/auth/Auth.provider.tsx
+++ client/src/data-providers/auth/Auth.provider.tsx
@@ -6,17 +6,17 @@ import { authContext } from "./auth.context";
 import { APIRoutes } from "@/navigation/routes/api.routes";
 
 // FOR DEVELOPMENT !!!
-const mockUser: IUser = {
-    name: "JohnDoeLongEmail@google.com",
-    roles: ["admin"]
-};
+// const mockUser: IUser = {
+//     name: "JohnDoeLongEmail@google.com",
+//     roles: ["admin"]
+// };
 
 interface IProps {
     children: React.ReactNode;
 }
 
 const AuthProvider: FC<IProps> = ({ children }) => {
-    const [user, setUser] = useState<IUser | null>(mockUser);
+    const [user, setUser] = useState<IUser | null>(null);
 
     const fetchUser = useCallback(async (): Promise<IUser | null> => {
         const timer = startLoading();
